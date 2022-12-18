010dedf059dba90590a262774a0f9ef38df59fd2
Bhagvendra Singh Parihar <91803092+Yashcodes@users.noreply.github.com>
2022-12-18 22:01:24 +0530

    Endpoint made : For Adding a note

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -16,4 +16,43 @@ router.get("/fetchallnotes", fetchUser, async (req, res) => {
   }
 });
 
+//? ROUTE 2
+//! Add a new note using : POST "api/notes/addnote". Login required
+router.post(
+  "/addnote",
+  fetchUser,
+  [
+    //! Validating the notes of user using express-validator
+
+    body("title", "Enter a valid title").isLength({ min: 3 }),
+    body("description", "Description must be at least 5 characters").isLength({
+      min: 5,
+    }),
+  ],
+  async (req, res) => {
+    try {
+      const { title, description, tag } = req.body;
+
+      //* Validation error resolve : If there are errors, return Bad request and the errors
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+
+      const note = new Notes({
+        title,
+        description,
+        tag,
+        user: req.user.id,
+      });
+
+      const savedNote = await note.save();
+      res.json(savedNote);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Internal Server Error");
+    }
+  }
+);
+
 module.exports = router;
