741c06052b12dafd2295d9eb70aa867599ac109e
Bhagvendra Singh Parihar <91803092+Yashcodes@users.noreply.github.com>
2022-12-25 19:29:51 +0530

    Changed Submit button to Add Note

--- src/components/AddNote.js
+++ src/components/AddNote.js
@@ -62,7 +62,7 @@ const AddNote = () => {
             className="btn btn-primary"
             onClick={handleClick}
           >
-            Submit
+            Add Note
           </button>
         </form>
       </div>
