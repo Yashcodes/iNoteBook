694ae27421dea0a99968a003f78b0c1cf51de661
Bhagvendra Singh Parihar <91803092+Yashcodes@users.noreply.github.com>
2023-01-02 11:32:33 +0530

    Added Login and SignUp Components | Login and SignUp Routes

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Home from "./components/Home";
 import About from "./components/About";
 import NoteState from "./context/notes/NoteState";
 import Alert from "./components/Alert";
+import Login from "./components/Login";
+import SignUp from "./components/SignUp";
 
 function App() {
   return (
@@ -12,11 +14,13 @@ function App() {
       <NoteState>
         <Router>
           <Navbar />
-          <Alert message = {'This is an alert'} />
+          <Alert message={"This is an alert"} />
           <div className="container">
             <Routes>
               <Route exact path="/" element={<Home />} />
               <Route exact path="/about" element={<About />} />
+              <Route exact path="/login" element={<Login />} />
+              <Route exact path="/signup" element={<SignUp />} />
             </Routes>
           </div>
         </Router>
---
+++ src/components/SignUp.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SignUp = () => {
+  return (
+    <div>
+      This is signup page
+    </div>
+  )
+}
+
+export default SignUp
